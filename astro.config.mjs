09c10865d2f8f8bd5aae1c896cee86a072c27ab2
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-08-31 01:17:04 +0530

    feat(2.2): Pass props to Astro components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-export default {
+// Full Astro Configuration API Documentation:
+// https://docs.astro.build/reference/configuration-reference
+
+// @type-check enabled!
+// VSCode and other TypeScript-enabled text editors will provide auto-completion,
+// helpful tooltips, and warnings if your exported object is invalid.
+// You can disable this by removing "@ts-check" and `@type` comments below.
+
+// @ts-check
+export default /** @type {import('astro').AstroUserConfig} */ {
   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // pages: './src/pages',   // Path to Astro components, pages, and data
   // dist: './dist',       // When running `astro build`, path to final static output
