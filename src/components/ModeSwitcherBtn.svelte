8a3e9dea82eb61d95febe5c1fe4e7430ac60b930
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-08-31 20:15:20 +0530

    feat(4.2): Encpasulate behaviour within component types

---
+++ src/components/ModeSwitcherBtn.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import ModeSwitcher from './ModeSwitcher.svelte'
+    import SvgIcon from './SvgIcon.svelte'
+</script>
+<ModeSwitcher let:theme>
+    <SvgIcon>
+        {#if theme === 'dark'}
+            <circle cx="12" cy="12" r="5"></circle>
+            <line x1="12" y1="1" x2="12" y2="3"></line>
+            <line x1="12" y1="21" x2="12" y2="23"></line>
+            <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+            <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+            <line x1="1" y1="12" x2="3" y2="12"></line>
+            <line x1="21" y1="12" x2="23" y2="12"></line>
+            <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+            <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+        {:else}
+            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+        {/if}
+    </SvgIcon>
+</ModeSwitcher>
