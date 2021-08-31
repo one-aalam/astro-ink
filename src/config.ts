aba7ac852eb96b96f4f9d29f5a5b12bf2844000a
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-08-31 08:26:36 +0530

    feat(3.1): Use local + static data

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+import type{ NavItems } from './types'
+
+export const NAV_ITEMS: NavItems = {
+    home: {
+        path: '/',
+        title: 'home'
+    },
+    about: {
+        path: '/about',
+        title: 'about'
+    }
+}
+
+export const SITE = {
+    // Your site's detail?
+    name: 'Ink',
+    title: 'Astro - Ink',
+    description: 'Crisp, minimal, personal blog theme for Astro',
+    url: 'https://astro-ink.vercel.app',
+    githubUrl: 'https://github.com/one-aalam/astro-ink'
+    // description ?
+}
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type NavItems = {
+    [key: string]: NavItem
+}
+
+export type NavItem = {
+    path: string
+    title: string
+}
