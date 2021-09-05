cdf73b9a8693747fde535966069910b171b0693b
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-09-05 17:27:26 +0530

    re-factor: getMonthName utility

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,11 @@
+const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+
 export const toTitleCase = (str: string) => str.replace(
       /\w\S*/g,
       function(txt) {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
       }
     )
+
+export const getMonthName = (date: Date) => MONTHS[new Date(date).getMonth()]
