2b39e1519906162ec05abcadd2d08f8849961de2
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-09-16 08:06:39 +0530

    feat: Add Netlify Admin + Identity

---
+++ src/components/NetlifyIdentity.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { onMount} from 'svelte'
+
+    onMount(() => {
+        if (window.netlifyIdentity) {
+            window.netlifyIdentity.on('init', (user) => {
+                if (!user) {
+                    window.netlifyIdentity.on('login', () => {
+                        document.location.href = '/admin/';
+                    });
+                }
+            });
+        }
+    })
+
+</script>
