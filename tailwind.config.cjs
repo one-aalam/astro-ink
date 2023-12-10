9a8093fb84f43c9d35bbd038ed9e41df927d71cc
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2023-12-10 17:26:44 +0530

    fix: remove deprecated Tailwind plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -72,6 +72,5 @@ module.exports = {
         require('@tailwindcss/typography'),
         require('@tailwindcss/forms'),
         require('@tailwindcss/aspect-ratio'),
-        require('tailwindcss-hyphens'),
     ]
 };
