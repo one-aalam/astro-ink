c83100b85d8e58a4a7912794e6c8737e0b15d9ae
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2023-12-10 19:03:38 +0530

    feat: `Netlify CMS` -> `Decap CMS`

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import markdoc from "@astrojs/markdoc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 import remarkCodeTitles from 'remark-code-titles'
+import decapCmsOauth from "astro-decap-cms-oauth";
 
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
@@ -37,13 +38,14 @@ export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
     tailwind({
       applyBaseStyles: false,
     }), 
-    sitemap(), 
+    sitemap(),
+    decapCmsOauth()
   ],
   vite: {
     plugins: [],
     resolve: {
       alias: {
-        '$': path.resolve(__dirname, './src')
+        $: path.resolve(__dirname, './src')
       }
     },
     optimizeDeps: {
