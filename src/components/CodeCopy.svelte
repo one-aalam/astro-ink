9c4db4cd024aa85eb31621144c6b69eec800d851
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2023-12-12 20:49:35 +0530

    fix: `CodeCopy` bug (a bit hackfully)

--- src/components/CodeCopy.svelte
+++ src/components/CodeCopy.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
     import { fade, blur } from "svelte/transition";
+    import { onMount } from "svelte";
     const COPIED_TIMEOUT = 2 * 1000
 
     export let stayCopied = COPIED_TIMEOUT
 
     let copied = false
     let selfElm: HTMLButtonElement;
+    let isCodeBlock = true;
 
     const copy = async () => {
         if(selfElm) {
             const preElm = selfElm.parentElement?.parentElement
             const codeElm = preElm?.querySelector('code')
-            if(codeElm) {
+            if(preElm?.tagName === 'PRE' && codeElm) {
                 await navigator.clipboard.writeText(codeElm.innerText);
                 copied = true
         
@@ -22,12 +24,22 @@
         }
         
     };
+
+    onMount(() => {
+        const preElm = selfElm.parentElement?.parentElement
+        if(preElm && preElm.tagName === 'PRE') {
+            isCodeBlock = true
+        } else {
+            isCodeBlock = false
+        }
+    })
 </script>
 <button 
     bind:this={selfElm} 
     on:click={copy} 
     class="absolute px-2 text-theme-primary dark:text-theme-dark-primary border-1 rounded-lg" 
     style="top: 6px; right: 8px;"
+    style:display={isCodeBlock ? 'inline-block' : 'none'}
     disabled={copied}
 >
     {#if copied}
